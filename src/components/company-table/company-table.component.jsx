a12fa7b7833d42ea33a426a575ef95f9e205781e
Jakob Arsement <jakobarsement@gmail.com>
2021-03-21 13:02:09 -0700

    Added company-table.component without APIs connected. Dashboard <div> in header still needs the buttons to stick to the botton of the parent <div>

---
+++ src/components/company-table/company-table.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./company-table.styles.scss";
+
+const CompanyTable = () => (
+	<div className="company-table">
+		<div className="table-cell">one</div>
+		<div className="table-cell">two</div>
+		<div className="table-cell">three</div>
+		<div className="table-cell">four</div>
+		<div className="table-cell">five</div>
+		<div className="table-cell">six</div>
+		<div className="table-cell">seven</div>
+		<div className="table-cell">eight</div>
+		<div className="table-cell">nine</div>
+		<div className="table-cell">ten</div>
+	</div>
+);
+
+export default CompanyTable;
--- src/pages/companypage/companypage.component.jsx
+++ src/pages/companypage/companypage.component.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import RightColumn from "../../components/right-column/right-column.component";
 import LeftColumn from "../../components/left-column/left-column.component";
+import CompanyTable from "../../components/company-table/company-table.component";
 
 import "./companypage.styles.scss";
 
 const CompanyPage = () => (
 	<div className="companypage">
 		<LeftColumn></LeftColumn>
-		<div className="middle-column"></div>
+		<div className="middle-column">
+			<CompanyTable></CompanyTable>
+		</div>
 		<RightColumn></RightColumn>
 	</div>
 );
