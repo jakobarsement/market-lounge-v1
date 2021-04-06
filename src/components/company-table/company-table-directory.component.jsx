3bfc04249c8e8a83d407a37c0f7c8db9d4840177
deepaktomar47 <deepak.tomar@47billion.com>
2021-04-06 19:47:58 +0530

    feat(simple-markets): integrated newsFeed and dashboard table

--- src/components/company-table/company-table-directory.component.jsx
+++ src/components/company-table/company-table-directory.component.jsx
@@ -6,13 +6,30 @@ import COMPANY_TABLE_DATA from "../../data/company-table-data";
 import "./company-table-directory.styles.scss";
 
 const CompanyTableDirectory = () => {
-	return (
-		<div className="company-table">
-			{COMPANY_TABLE_DATA.map((cellN) => (
-				<TableCell key={cellN.id} {...cellN} />
-			))}
-		</div>
-	);
+  const renderTableOne = () => {
+    return COMPANY_TABLE_DATA.map((cellN) => {
+      const { id } = cellN;
+      if (id <= 5) {
+        return <TableCell key={"tableOne" + id} id={id} {...cellN} />;
+      }
+    });
+  };
+
+  const renderTableTwo = () => {
+    return COMPANY_TABLE_DATA.map((cellN) => {
+      const { id } = cellN;
+      if (id > 5) {
+        return <TableCell key={"tableTwo" + id} id={id} {...cellN} />;
+      }
+    });
+  };
+
+  return (
+    <>
+      <div className="company-table">{renderTableOne()}</div>
+      <div className="company-table">{renderTableTwo()}</div>
+    </>
+  );
 };
 
 export default CompanyTableDirectory;
