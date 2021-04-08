31aea40b49d4ff2233cf30637f25be48d29eb388
Rohan Vaghani <rohanvaghani@gmail.com>
2021-04-08 12:10:56 -0400

    donate button

--- src/components/feedback/feedback.component.jsx
+++ src/components/feedback/feedback.component.jsx
@@ -58,6 +58,12 @@ const Feedback = () => {
         onChange={e => setAmount(e.target.value)}
       />
 		  <Paypal amount={amount} />
+      <form action="https://www.paypal.com/donate" method="post" target="_blank">
+        <input type="hidden" name="business" value="BHM6A6NVZBNC6" />
+        <input type="hidden" name="currency_code" value="USD" />
+        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
+      </form>
 			{submitted && (
 				<div className="success-message">
 					SUCCESS<div className="sub-success-message">Talk soon!</div>
