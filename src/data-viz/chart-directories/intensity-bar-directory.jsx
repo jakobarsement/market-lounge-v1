532ba665aa5f03612dd2a139ffd9e802acba1256
jakobarsement <43357341+jakobarsement@users.noreply.github.com>
2021-04-25 12:35:47 -0400

    testing

--- src/data-viz/chart-directories/intensity-bar-directory.jsx
+++ src/data-viz/chart-directories/intensity-bar-directory.jsx
@@ -4,45 +4,45 @@ import { useFetchData } from "../hooks/useFetchData";
 
 import "./intensity-bar.styles.scss";
 export const FinancialGrowth = ({ company }) => {
-  const url = `https://financialmodelingprep.com/api/v3/financial-growth/${company}?period=quarter&limit=80&apikey=7fd4e8b6bf2bceea94a8f589d648c8eb`;
+	const url = `https://financialmodelingprep.com/api/v3/financial-growth/${company}?period=quarter&limit=80&apikey=7fd4e8b6bf2bceea94a8f589d648c8eb`;
 
-  const formatData = useCallback((data) => {
-    const lastEightQuarters = data
-      .slice(0, 16)
-      .filter((data, index) => index % 2 === 0);
-    return lastEightQuarters.map(
-      ({ date, period, revenueGrowth, ebitgrowth }) => ({
-        date,
-        period,
-        revenueGrowth: Math.round(revenueGrowth * 100) / 100,
-        ebitgrowth: Math.round(ebitgrowth * 100) / 100,
-        formattedDate: `${period}'${date.split("-")[0].substring(2, 4)}`,
-      })
-    );
-  }, []);
+	const formatData = useCallback((data) => {
+		const lastEightQuarters = data
+			.slice(0, 16)
+			.filter((data, index) => index % 2 === 0);
+		return lastEightQuarters.map(
+			({ date, period, revenueGrowth, ebitgrowth }) => ({
+				date,
+				period,
+				revenueGrowth: Math.round(revenueGrowth * 100) / 100,
+				ebitgrowth: Math.round(ebitgrowth * 100) / 100,
+				formattedDate: `${period}'${date.split("-")[0].substring(2, 4)}`,
+			})
+		);
+	}, []);
 
-  const { status, data } = useFetchData(url, company, formatData);
+	const { status, data } = useFetchData(url, company, formatData);
 
-  if (status !== "DONE") {
-    return null;
-  }
+	if (status !== "DONE") {
+		return null;
+	}
 
-  return (
-    <div className="intensity-bars">
-      <IntensityBar
-        key="revenueGrowth"
-        company={company}
-        indicator="revenueGrowth"
-        title="Q/Q Revenue Growth"
-        chartData={data}
-      />
-      <IntensityBar
-        key="ebitgrowth"
-        company={company}
-        indicator="ebitgrowth"
-        title="Q/Q EBIT Growth"
-        chartData={data}
-      />
-    </div>
-  );
+	return (
+		<div className="intensity-bars">
+			<IntensityBar
+				key="revenueGrowth"
+				company={company}
+				indicator="revenueGrowth"
+				title="Q/Q Revenue Growth"
+				chartData={data}
+			/>
+			<IntensityBar
+				key="ebitgrowth"
+				company={company}
+				indicator="ebitgrowth"
+				title="Q/Q EBIT Growth"
+				chartData={data}
+			/>
+		</div>
+	);
 };
--- src/data-viz/graphs/company-ratio-chart.jsx
+++ src/data-viz/graphs/company-ratio-chart.jsx
@@ -3,48 +3,48 @@ import Highcharts from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
 
 export const RatioLineChart = ({
-  company,
-  chartData,
-  indicator,
-  yAxisLabel,
+	company,
+	chartData,
+	indicator,
+	yAxisLabel,
 }) => {
-  const options = {
-    chart: {
-      type: "line",
-      height: "200px",
-    },
-    title: {
-      text: "",
-    },
-    xAxis: {
-      categories: chartData.map((data) => data.formattedDate),
-    },
-    yAxis: {
-      title: {
-        text: yAxisLabel,
-      },
-    },
-    plotOptions: {
-      line: {
-        dataLabels: {
-          enabled: true,
-        },
-        enableMouseTracking: true,
-      },
-    },
-    series: [
-      {
-        name: company,
-        data: chartData.map((data) => data[indicator]),
-        color: "blue",
-      },
-    ],
-  };
+	const options = {
+		chart: {
+			type: "line",
+			height: "200px",
+		},
+		title: {
+			text: "",
+		},
+		xAxis: {
+			categories: chartData.map((data) => data.formattedDate),
+		},
+		yAxis: {
+			title: {
+				text: yAxisLabel,
+			},
+		},
+		plotOptions: {
+			line: {
+				dataLabels: {
+					enabled: true,
+				},
+				enableMouseTracking: true,
+			},
+		},
+		series: [
+			{
+				name: company,
+				data: chartData.map((data) => data[indicator]),
+				color: "blue",
+			},
+		],
+	};
 
-  console.log(options);
-  return (
-    <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
-    </div>
-  );
+	console.log(options);
+	return (
+		<div>
+			<HighchartsReact highcharts={Highcharts} options={options} />
+		</div>
+	);
 };
