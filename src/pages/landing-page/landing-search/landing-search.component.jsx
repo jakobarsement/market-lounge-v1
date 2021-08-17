4965c9ecf24ba97c6d557151637d435324b02070
jakobarsement <43357341+jakobarsement@users.noreply.github.com>
2021-08-16 21:40:33 -0700

    Format landing page title

--- src/pages/landing-page/landing-search/landing-search.component.jsx
+++ src/pages/landing-page/landing-search/landing-search.component.jsx
@@ -26,13 +26,15 @@ const LandingSearch = () => {
 	return (
 		<div className="main">
 			<div className="content">
-				<h1 className="title">Simple Markets</h1>
+				<h1 className="title">
+					<span class="the">the</span> market lounge.
+				</h1>
 
 				<div className="searchbar-container">
 					<i className="fas fa-search fa-2x search-icon"></i>
 					<input
 						type="text"
-						placeholder="Search a company..."
+						placeholder="search a company by ticker..."
 						className="searchbar"
 						ref={inputRef}
 						onKeyPress={handleSubmit}
