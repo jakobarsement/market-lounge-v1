92ff59707a12859c5be157d26d637278261624d1
jakobarsement <43357341+jakobarsement@users.noreply.github.com>
2021-09-03 21:10:51 -0700

    convert landing page bg img to webp compression

--- src/pages/landing-page/landing-page.component.tsx
+++ src/pages/landing-page/landing-page.component.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-
-import HeaderMain from "../../components/header/header-main.component";
 import LandingSearch from "./landing-search/landing-search.component";
 
 import "./landing-page.styles.scss";
 
 const LandingPage = () => (
 	<div className="landing-page">
-		{/* <HeaderMain></HeaderMain> */}
 		<LandingSearch></LandingSearch>
 	</div>
 );
