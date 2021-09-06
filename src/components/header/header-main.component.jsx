ffa6f1b9b2a445750507b4b4122286c349e4069c
Jakob Arsement <jakobarsement@gmail.com>
2021-09-05 21:48:39 -0700

    add todo; prettier will start formatting tabs to spaces

--- src/components/header/header-main.component.jsx
+++ src/components/header/header-main.component.jsx
@@ -3,37 +3,36 @@ import { Link } from "react-router-dom";
 
 import "./header-main.styles.scss";
 const HeaderMain = (props) => {
-	return (
-		<div
-			className="header-main"
-			style={{
-				backgroundColor:
-					window.location.pathname === "/"
-						? "rgba(0,0,0, 0)"
-						: "rgb(47, 47, 42)",
-			}}
-		>
-			{console.log(window.location.pathname)}
-			<div className="logo-left">
-				<Link className="logo-container" to="/">
-					{/*  Logo Placeholder */}
-				</Link>
-			</div>
-			<div className="header-options">
-				<Link className="option" to="/">
-					<h4>Home</h4>
-				</Link>
-				<Link className="option" to="/companypage">
-					<h4>Company</h4>
-				</Link>
-				<Link className="option" to="/companypage">
-					<h4>Books</h4>
-				</Link>
-				<Link className="option" to="/companypage">
-					<h4>About Us</h4>
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="header-main"
+      style={{
+        backgroundColor:
+          window.location.pathname === "/"
+            ? "rgba(0,0,0, 0)"
+            : "rgb(47, 47, 42)",
+      }}
+    >
+      <div className="logo-left">
+        <Link className="logo-container" to="/">
+          {/*  Logo Placeholder */}
+        </Link>
+      </div>
+      <div className="header-options">
+        <Link className="option" to="/">
+          <h4>Home</h4>
+        </Link>
+        <Link className="option" to="/companypage">
+          <h4>Company</h4>
+        </Link>
+        <Link className="option" to="/companypage">
+          <h4>Books</h4>
+        </Link>
+        <Link className="option" to="/companypage">
+          <h4>About Us</h4>
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default HeaderMain;
--- src/components/middle-column/company-header/company-header.component.jsx
+++ src/components/middle-column/company-header/company-header.component.jsx
@@ -6,62 +6,63 @@ import _ from "lodash";
 import "./company-header.styles.scss";
 
 function CompanyHeader() {
-	const inputRef = useRef(null);
-	const { companySymbol, setCompanySymbol } = useContext(CompanySymbolContext);
-	const [summaryData, setSummaryData] = useState();
-	const [isDescriptionShown, setIsDescriptionShown] = useState(false);
+  const inputRef = useRef(null);
+  const { companySymbol, setCompanySymbol } = useContext(CompanySymbolContext);
+  const [summaryData, setSummaryData] = useState();
+  const [isDescriptionShown, setIsDescriptionShown] = useState(false);
 
-	const summaryUrl = `https://financialmodelingprep.com/api/v3/profile/${companySymbol}?apikey=7fd4e8b6bf2bceea94a8f589d648c8eb`;
+  const summaryUrl = `https://financialmodelingprep.com/api/v3/profile/${companySymbol}?apikey=7fd4e8b6bf2bceea94a8f589d648c8eb`;
 
-	useEffect(() => {
-		if (!summaryUrl) return;
-		const fetchData = async () => {
-			const response = await fetch(summaryUrl);
-			const data = await response.json();
-			setSummaryData(data);
-		};
-		fetchData();
-	}, [summaryUrl]);
+  useEffect(() => {
+    if (!summaryUrl) return;
+    const fetchData = async () => {
+      const response = await fetch(summaryUrl);
+      const data = await response.json();
+      setSummaryData(data);
+    };
+    fetchData();
+  }, [summaryUrl]);
 
-	const handleSubmit = (e) => {
-		if (e.key === "Enter" && inputRef.current.value) {
-			const uppercaseCompanySymbol = _.upperCase(inputRef.current.value);
-			setCompanySymbol(uppercaseCompanySymbol);
-		}
-	};
-	return (
-		<div className="company-header">
-			<div className="company-search-container">
-				<i className="fas fa-search"></i>
-				<input
-					className="company-input-box"
-					ref={inputRef}
-					onKeyPress={handleSubmit}
-				/>
-			</div>
-			<div className="company-details">
-				<h2>
-					{companySymbol}
-					<span className="description-icon">
-						<FontAwesomeIcon
-							onMouseEnter={() => setIsDescriptionShown(true)}
-							onMouseLeave={() => setIsDescriptionShown(false)}
-							className="fa-info-circle"
-							icon={faInfoCircle}
-						></FontAwesomeIcon>
-					</span>
-				</h2>
-				{isDescriptionShown && (
-					<p className="company-description">{summaryData[0].description}</p>
-				)}
-				{!!summaryData && (
-					<p>
-						<strong>CEO: </strong> <span>{summaryData[0].ceo}</span>{" "}
-					</p>
-				)}
-			</div>
-		</div>
-	);
+  const handleSubmit = (e) => {
+    if (e.key === "Enter" && inputRef.current.value) {
+      const uppercaseCompanySymbol = _.upperCase(inputRef.current.value);
+      setCompanySymbol(uppercaseCompanySymbol);
+    }
+  };
+  return (
+    <div className="company-header">
+      <div className="company-search-container">
+        <i className="fas fa-search"></i>
+        <input
+          //TODO: on state change implement search -> https://financialmodelingprep.com/developer/docs#Ticker-Search
+          className="company-input-box"
+          ref={inputRef}
+          onKeyPress={handleSubmit}
+        />
+      </div>
+      <div className="company-details">
+        <h2>
+          {companySymbol}
+          <span className="description-icon">
+            <FontAwesomeIcon
+              onMouseEnter={() => setIsDescriptionShown(true)}
+              onMouseLeave={() => setIsDescriptionShown(false)}
+              className="fa-info-circle"
+              icon={faInfoCircle}
+            ></FontAwesomeIcon>
+          </span>
+        </h2>
+        {isDescriptionShown && (
+          <p className="company-description">{summaryData[0].description}</p>
+        )}
+        {!!summaryData && (
+          <p>
+            <strong>CEO: </strong> <span>{summaryData[0].ceo}</span>{" "}
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default CompanyHeader;
