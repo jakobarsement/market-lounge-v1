a362eac7a5f412f43920636ce0d61ee2a89a514d
jakobarsement <jakobarsement@gmail.com>
2021-09-08 22:50:14 -0700

    make landing-search stick to middle of screen

--- src/pages/landing-page/landing-page.component.tsx
+++ src/pages/landing-page/landing-page.component.tsx
@@ -4,10 +4,10 @@ import LandingSearch from "./landing-search/landing-search.component";
 import "./landing-page.styles.scss";
 
 const LandingPage = () => (
-	<div className="landing-page">
-		<LandingSearch></LandingSearch>
-		<div className="bg-img"></div>
-	</div>
+  <div className="landing-page">
+    <LandingSearch></LandingSearch>
+    <div className="bg-img"></div>
+  </div>
 );
 
 export default LandingPage;
