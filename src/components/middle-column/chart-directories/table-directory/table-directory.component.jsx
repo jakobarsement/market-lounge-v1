a6aea8424e96e8e53b903f4b2af34bdb99c3d4d0
jakobarsement <jakobarsement@gmail.com>
2021-09-26 15:26:39 -0700

    checking temp changes

--- src/components/middle-column/chart-directories/table-directory/table-directory.component.jsx
+++ src/components/middle-column/chart-directories/table-directory/table-directory.component.jsx
@@ -6,15 +6,15 @@ import getInterpolatedApiUrl from "../../../../data/company-table-data";
 import "./table-directory.styles.scss";
 
 const CompanyTableDirectory = ({ companySymbol }) => {
-	const renderTableOne = () => {
-		const company_data = getInterpolatedApiUrl(companySymbol);
+  const renderTableOne = () => {
+    const company_data = getInterpolatedApiUrl(companySymbol);
 
-		return company_data.map((cellN) => {
-			const { id } = cellN;
-			return <TableCell key={"tableOne" + id} id={id} {...cellN} />;
-		});
-	};
-	return <div className="company-table">{renderTableOne()}</div>;
+    return company_data.map((cellN) => {
+      const { id } = cellN;
+      return <TableCell key={"tableOne" + id} id={id} {...cellN} />;
+    });
+  };
+  return <div className="company-table">{renderTableOne()}</div>;
 };
 
 export default CompanyTableDirectory;
