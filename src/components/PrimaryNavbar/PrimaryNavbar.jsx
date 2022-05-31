99e49e41489f301fff1f0c41b3cc8ca7ddee5446
Jon DiVito <you@example.com>
2022-05-30 20:39:07 -0500

    fix scrollbar bug and restyle scrollbar to fit design motif of site

--- src/components/PrimaryNavbar/PrimaryNavbar.jsx
+++ src/components/PrimaryNavbar/PrimaryNavbar.jsx
@@ -1,41 +1,49 @@
-import LoginButton from 'components/LoginButton'
-import LogoutButton from 'components/LogoutButton'
-import { Link } from 'react-router-dom'
-import { useAuth0 } from '@auth0/auth0-react'
-import { API } from 'aws-amplify'
+import LoginButton from 'components/LoginButton';
+import LogoutButton from 'components/LogoutButton';
+import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import { API } from 'aws-amplify';
 
-import './PrimaryNavbar.scss'
+import './PrimaryNavbar.scss';
 
 const PrimaryNavbar = () => {
-  const { user, isAuthenticated } = useAuth0()
+  const { user, isAuthenticated } = useAuth0();
 
-  if (user) API.post('mailchimpApi', '/mailchimpApi', { body: { email: user.email } })
+  if (user)
+    API.post('mailchimpApi', '/mailchimpApi', { body: { email: user.email } });
 
   return (
-    <div className="header-main">
-      <div className="logo-left">
-        <Link className="logo-container" to="/">
+    <div className='header-main'>
+      <div className='logo-left'>
+        <Link className='logo-container' to='/'>
           {/*  Logo Placeholder */}
         </Link>
       </div>
-      <div className="header-options">
-        <Link className="option" to="/">
+      <div className='header-options'>
+        <Link className='option' to='/'>
           <h4>Home</h4>
         </Link>
-        <Link className="option" to={window.location.pathname !== '/company' ? '/company' : null}>
+        <Link
+          className='option'
+          to={window.location.pathname !== '/company' ? '/company' : null}
+        >
           <h4>Company</h4>
         </Link>
-        <Link className="option" to="/companypage">
+        <Link className='option' to='/companypage'>
           <h4>Books</h4>
         </Link>
-        <Link className="option" to="/companypage">
+        <Link className='option' to='/companypage'>
           <h4>About Us</h4>
         </Link>
-        <div className="auth-options">
-          {isAuthenticated ? <LogoutButton className="logout" /> : <LoginButton className="login" />}
+        <div className='auth-options'>
+          {isAuthenticated ? (
+            <LogoutButton className='logout' />
+          ) : (
+            <LoginButton className='login' />
+          )}
         </div>
       </div>
     </div>
-  )
-}
-export default PrimaryNavbar
+  );
+};
+export default PrimaryNavbar;
